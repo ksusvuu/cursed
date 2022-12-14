b30e79792980d62c662e280dcb92bdf83a8c90b9
Den <denis100703@gmail.com>
2022-12-15 01:42:47 +0300

    files update

--- main.cpp
+++ main.cpp
@@ -2,28 +2,27 @@
 
 using namespace std;
 
-
-// Driver program to test above function
+// driver program to test function
 // dot -Tsvg -O file.dot
 // ./file.dot.svg
-int main()
-{
-    Graph test_graph(4);
-    test_graph.set_matrix(".matrix");
-    // test_graph.addEdge(0, 1);
-    // test_graph.addEdge(0, 2);
-    // test_graph.addEdge(1, 2);
-    // test_graph.addEdge(1, 3);
-    // test_graph.addEdge(2, 3);
-    // test_graph.addEdge(3, 4);
-    cout << "Coloring of graph" << endl;
-    // test_graph.rlf_coloring();
-    test_graph.greedy_сoloring();
-    test_graph.chromatic_number();
-    test_graph.print_matrix();
-    test_graph.draw_graph("file.dot");
-    
-    test_graph.print_chromatic_number();
-  
-    return 0;
+
+int main() {
+  Graph test_graph(6);
+  test_graph.set_matrix(".matrix");
+  // test_graph.addEdge(0, 1);
+  // test_graph.addEdge(0, 2);
+  // test_graph.addEdge(1, 2);
+  // test_graph.addEdge(1, 3);
+  // test_graph.addEdge(2, 3);
+  // test_graph.addEdge(3, 4);
+  cout << "Coloring of graph" << endl;
+  // test_graph.rlf_coloring();
+  test_graph.greedy_coloring();
+  test_graph.chromatic_number();
+  test_graph.print_matrix();
+  test_graph.draw_graph("file.dot");
+
+  test_graph.print_chromatic_number();
+
+  return 0;
 }
